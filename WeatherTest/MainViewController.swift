5eb7f5767ddb5bdaf8f7837eed9994ab318f5cbc
Gaurav Konchady <gaurav.s.konchady@medtronic.com>
2016-06-09 16:20:38 -0500

    test comment 4

--- WeatherTest/MainViewController.swift
+++ WeatherTest/MainViewController.swift
@@ -23,7 +23,7 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("test comment 3")
+        print("test comment 4")
         
         //Read url and key from your Settings plist file
         var myDict: NSDictionary?
